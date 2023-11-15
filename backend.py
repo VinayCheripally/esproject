cf1f6758124f4363540536dea03e51871f8182b8
VinayCheripally <vinaycheripally1@gmail.com>
2023-11-15 16:08:05 +0530

    commit

--- backend.py
+++ backend.py
@@ -1,10 +1,14 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route('/phDetection')
+@app.route('/phDetection',methods=['GET','POST'])
 def pred():
-    return render_template('home.html')
+
+    x= 0
+    if 'text' in request.form and request.method=='POST':
+        x = 7
+    return render_template('home.html',inp = x)
 
 if __name__ == "__main__":
     app.run(debug=True)
