d636740e618d0eec434740af8c174d339d5ba290
VinayCheripally <vinaycheripally1@gmail.com>
2023-11-15 16:52:22 +0530

    add

--- backend.py
+++ backend.py
@@ -1,13 +1,17 @@
 from flask import Flask,render_template,request
-
+from modules.ml import ml
 app = Flask(__name__)
 
 @app.route('/phDetection',methods=['GET','POST'])
 def pred():
 
-    x= 0
-    if 'text' in request.form and request.method=='POST':
-        x = 7
+    x= "Please specify the number of days for predicting rainfall's pH"
+    if request.method=='POST' and 'text' in request.form:
+        value = request.form["text"]
+        print(value)
+        # ml_obj  = ml()
+        # x = ml_obj.runQuery(value)
+        x = 8
     return render_template('home.html',inp = x)
 
 if __name__ == "__main__":
